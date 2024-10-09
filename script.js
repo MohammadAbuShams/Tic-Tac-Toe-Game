f4f704602a5f3d905adafb67c727118263814a4f
Mohammad AbuShams <116977597+MohammadAbuShams@users.noreply.github.com>
2024-10-10 02:05:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.querySelector('.game-board');
+const winningMessageElement = document.querySelector('.winning-message');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const restartButton = document.getElementById('restartButton');
+let oTurn;
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+    oTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    winningMessageElement.style.display = 'none';
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+    console.log(`Cell clicked: ${cell} | Current Class: ${currentClass}`);
+}
+
+function placeMark(cell, currentClass) {
+    cell.textContent = currentClass; 
+    cell.classList.add(currentClass);
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = "It's a draw!";
+    } else {
+        winningMessageTextElement.innerText = `${oTurn ? "O's" : "X's"} Wins!`;
+    }
+    winningMessageElement.style.display = 'block';
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+    });
+}
+
+function swapTurns() {
+    oTurn = !oTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(O_CLASS);
+    if (oTurn) {
+        board.classList.add(O_CLASS);
+    } else {
+        board.classList.add(X_CLASS);
+    }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        });
+    });
+}
